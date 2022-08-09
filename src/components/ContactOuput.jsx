71b8cee8f8f3798c94411f2a901f63cc7037576e
COValhalla <josephvellella@proton.me>
2022-08-09 11:06:29 -0600

    Center text on enter, better user experience.

--- src/components/ContactOuput.jsx
+++ src/components/ContactOuput.jsx
@@ -5,7 +5,7 @@ export default function ContactOutput(props) {
     <div className="flex  flex-col place-items-center justify-center gap-2 bg-orange-500 py-4">
       <h1 className="text-3xl font-bold">{props.name}</h1>
       <div>
-        <h1 className="italic">{props.address}</h1>
+        <h1 className="text-center italic">{props.address}</h1>
         <h1 className="flex gap-4 italic">
           <a href={`mailto:${props.email}`}>{props.email}</a>
           <a href={`tel:${props.phone}`}>{props.phone}</a>
